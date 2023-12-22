5d1c4532dfa435f759207d57bc450f18fdbb2713
caadiq <caadiq@gmail.com>
2023-12-22 11:57:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/beemer/fromis9server/Fromis9ServerApplication.java
@@ -0,0 +1,13 @@
+package com.beemer.fromis9server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fromis9ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fromis9ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beemer/fromis9server/Fromis9ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beemer.fromis9server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fromis9ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
