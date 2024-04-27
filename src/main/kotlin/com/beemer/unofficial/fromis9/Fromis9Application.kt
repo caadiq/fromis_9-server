ce72b0a1fbfca5b4b8c4a0a90c0a4070888a20a1
caadiq <caadiq@gmail.com>
2024-04-27 12:22:47 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/beemer/unofficial/fromis9/Fromis9Application.kt
@@ -0,0 +1,11 @@
+package com.beemer.unofficial.fromis9
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Fromis9Application
+
+fun main(args: Array<String>) {
+	runApplication<Fromis9Application>(*args)
+}
---
+++ src/test/kotlin/com/beemer/unofficial/fromis9/Fromis9ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.beemer.unofficial.fromis9
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Fromis9ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
