bf4aaf8da210f28892a88570f42271ad569192a5
반명우 <caadiq@gmail.com>
2024-12-29 09:47:08 +0900

    fastapi 테스트

--- src/main/kotlin/com/beemer/unofficial/fromis9/youtube/scheduler/YouTubeScheduler.kt
+++ src/main/kotlin/com/beemer/unofficial/fromis9/youtube/scheduler/YouTubeScheduler.kt
@@ -1,11 +1,17 @@
 package com.beemer.unofficial.fromis9.youtube.scheduler
 
 import com.beemer.unofficial.fromis9.youtube.service.YouTubeService
+import jakarta.annotation.PostConstruct
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class YouTubeScheduler(private val youtubeService: YouTubeService) {
+
+    @PostConstruct
+    fun init() {
+        youtubeService.getVideoDetails()
+    }
 
     @Scheduled(cron = "0 0,20,40 * * * *")
     fun getVideoDetails() {
